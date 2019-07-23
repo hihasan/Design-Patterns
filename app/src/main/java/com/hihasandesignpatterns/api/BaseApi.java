c376e48ecffc8091a00251d41c66cd8853128608
hihasan <hasan@hihasan.com>
2019-07-23 12:13:46 +0600

    retrofit

--- app/src/main/java/com/hihasandesignpatterns/api/BaseApi.java
+++ app/src/main/java/com/hihasandesignpatterns/api/BaseApi.java
@@ -1,10 +1,4 @@
-//https://api.github.com/users
-
 package com.hihasandesignpatterns.api;
 
-
-
-public class BaseApi
-{
-    public static String BaseUrl="https://api.github.com/users";
+public interface BaseApi {
 }
