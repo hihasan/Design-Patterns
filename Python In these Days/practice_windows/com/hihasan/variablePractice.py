2973659c9eff6a7669273c1cee9b03b001d44e02
hihasan <hasan@hihasan.com>
2019-10-19 02:24:13 +0600

    practice_windows

---
+++ Python In these Days/practice_windows/com/hihasan/variablePractice.py
@@ -0,0 +1,39 @@
+import this
+
+
+print("Hello World !");
+
+message ="Hello World Meesage";
+print(message)
+
+# I will do some String Practice in here
+# In programming String handling is the most crucial
+# You need to understand one thing
+# An user can see everything in string resources
+message='i Asked Tasmiah,"DO You Love me?"'
+print(message)
+print(message.capitalize())
+print(message.title())
+print(message.lower())
+print(message.upper())
+
+#Concatination
+fname="Hasan"
+lname='Al Mamun'
+
+print(fname +" "+lname)
+print("Hello, "+fname.title())
+
+# Tab & Newline
+message = "Name: \tHasan, \tMamun, \tNadim, \tTasmiah"
+print(message.title())
+
+message = "Name: \nHasan, \nMamun, \nNadim, \nTasmiah"
+print(message.title())
+
+age=23
+#print("It is Hasan's"+age+"rd Birthday") #TypeError: can only concatenate str (not "int") to str
+print("It is Hasan's "+str(age)+"rd Birthday");
+
+print(1/2)
+
