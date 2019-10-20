6a2a38de9e69f041c37cdb444ce6962b5cacecba
hihasan <hasan@hihasan.com>
2019-10-20 12:10:51 +0600

    list Practice

---
+++ Python In these Days/practice_linux/com/hihasan/ListPractice.py
@@ -0,0 +1,9 @@
+name=["Hasan","Mamun","Nadim"]
+name2=["Rony","Rasel"]
+print(name[0])
+del_value=name.pop()
+print(name)
+print("Pop value: " +del_value)
+print(name2)
+name2.append(del_value)
+print(name2)
