f56b848c402303a168f992ac473bc91094efcc50
hihasan <hasan@hihasan.com>
2019-10-20 12:19:11 +0600

    list

--- Python In these Days/practice_linux/com/hihasan/ListPractice.py
+++ Python In these Days/practice_linux/com/hihasan/ListPractice.py
@@ -1,5 +1,9 @@
+#list-variavle
 name=["Hasan","Mamun","Nadim"]
 name2=["Rony","Rasel"]
+number=[1,4,6,2,3,2,90,23]
+
+
 print(name[0])
 del_value=name.pop()
 print(name)
@@ -7,3 +11,12 @@ print("Pop value: " +del_value)
 print(name2)
 name2.append(del_value)
 print(name2)
+name2.sort()
+print(name2)
+number.sort()
+print(number)
+# number.sort(reverse=true)
+# print(number)
+
+#Length of the list
+print("Length of the list: "+str(len(number)))
