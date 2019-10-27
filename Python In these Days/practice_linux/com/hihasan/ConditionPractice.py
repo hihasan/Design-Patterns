34508eea8543cf4e9c7cad5b3e2dd7f3d5bd7220
hihasan <hasan@hihasan.com>
2019-10-27 16:42:16 +0600

    files

--- Python In these Days/practice_linux/com/hihasan/ConditionPractice.py
+++ Python In these Days/practice_linux/com/hihasan/ConditionPractice.py
@@ -1,8 +1,40 @@
-name=["hasan","mamun","nadim"]
-numbers=[1,2,3,4,5,6,7,9,2]
+# name=["hasan","mamun","nadim"]
+# numbers=[1,2,3,4,5,6,7,9,2]
+#
+# for n in numbers:
+#     if n%2==0:
+#         print(n)
+#     else:
+#         print("Odd Number")
+#
+# cars=["Bmw","honda","toyota"]
 
-for n in numbers:
-    if n%2==0:
+# for car in cars:
+#     if car=='toyota':
+#         print(car.upper())
+#     else:
+#         print(car.lower())
+#
+# car='Audi'
+# print(car.lower()=='audi')
+#
+# print("\n\n")
+#
+# price=0;
+# age=12;
+#
+# if age<4:
+#     price=0
+# elif age>4:
+#     price=15
+# else:
+#     price=5
+#
+# print("Price acccording to your age: "+str(price))
+
+number=[2,3,6,9,13]
+
+for n in number:
+    if n%n==1:
         print(n)
-    else:
-        print("Odd Number")
+
