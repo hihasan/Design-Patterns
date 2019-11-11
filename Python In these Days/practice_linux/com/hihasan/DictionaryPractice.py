2b73f9894cb1d671d2f1dbcf5e9254384139d535
hihasan <hasan@hihasan.com>
2019-11-11 15:03:44 +0600

    inpuit & while

--- Python In these Days/practice_linux/com/hihasan/DictionaryPractice.py
+++ Python In these Days/practice_linux/com/hihasan/DictionaryPractice.py
@@ -1,3 +1,12 @@
-test={'color':'Green','point':'5'}
-print(test['color'])
-print(test['point'])
+# test={'color':'Green','point':'5'}
+# print(test['color'])
+# print(test['point'])
+
+test={'color':'green'}
+# print("The color is : "+test['color'])
+
+# test={'color':'yellow'}
+# print("Now The Color is: "+test['color'])
+
+test={'x_position':'1','y_position':2,'speed':'medium'}
+print(test)
---
+++ Python In these Days/practice_linux/com/hihasan/iopractice.py
@@ -0,0 +1,13 @@
+# x=input()
+# y=input()
+# z=x+y
+# z=int(z)
+# print("The SUmmation is: "+int(z))
+
+# While Loop
+
+number=1;
+
+while number<=5:
+    print(number)
+    number+=1
