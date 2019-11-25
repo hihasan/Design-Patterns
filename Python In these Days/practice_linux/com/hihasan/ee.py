f4e84f3dd9f2c3965917c723629ac66a3dde6caa
hihasan <hasan@hihasan.com>
2019-11-25 18:01:27 +0600

    function

---
+++ Python In these Days/practice_linux/com/hihasan/ee.py
@@ -0,0 +1,16 @@
+# def greet(n):
+#     print("Hello, " + n);
+#
+# greet("Hasan")
+
+# def name(fname,lname):
+#     print("My First Name is: "+fname)
+#     print("My Last Name is: "+lname)
+#
+# name("Hasan","Mamun")
+
+def num(a,b):
+    c=a+b
+    return print(c)
+
+num(2,3)
