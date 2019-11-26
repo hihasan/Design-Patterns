9d397f8b0a0a1c51dfb7d86779ed24d651e540c0
hihasan <hasan@hihasan.com>
2019-11-26 10:32:37 +0600

    python

---
+++ Python In these Days/practice_problem/practice/2_practice.py
@@ -0,0 +1,29 @@
+#2.1
+message="Hasan"
+print("Hello, "+message)
+
+#2.2
+message="Hasan"
+print("Hello, "+message)
+message="Mamun"
+print("Hello, "+message)
+
+#2.3
+name = "Hasan"
+print("Hello "+name+", do you like to learn Python Today?")
+
+#2.4
+name="Hasan"
+print("Lowercase: "+name.lower())
+print("Upercase: "+name.upper())
+print("TitleCase: "+name.title())
+
+#2.5
+name="Abert Einstain"
+message=" once said, “A person who never made a mistake never tried anything new.”"
+print(name +  message)
+
+#2.6
+fperson_name="Abert Einstain"
+message=" once said, “A person who never made a mistake never tried anything new.”"
+print(fperson_name +  message)
