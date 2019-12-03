2b2baae05828ab0f9d9a7b755099d230f37a0145
hihasan <hasan@hihasan.com>
2019-12-03 18:13:29 +0600

    test map

--- Python In these Days/practice_windows/com/hihasan/test.py
+++ Python In these Days/practice_windows/com/hihasan/test.py
@@ -1,1 +1,14 @@
-print ("Hello")
+def square(num):
+    return num**3
+# square(3)
+seq=[1,2,3,4]
+value = list(map(square,seq))
+
+for i in value:
+    print ('{}'.format(i))
+
+# for i in value:
+#     print('The Qbic Value of {} is: {}'.format(seq,i))
+#
+# for i in seq:
+#     print(i)
